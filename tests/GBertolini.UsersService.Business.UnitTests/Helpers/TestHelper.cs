59f0a309d49553e3c50b7936121d14e0d1e679ce
German Bertolini <german.bertolini@gmail.com>
2023-03-05 23:20:56 -0300

    unit test added for business layer

---
+++ tests/GBertolini.UsersService.Business.UnitTests/Helpers/TestHelper.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using GBertolini.UsersService.Models.AutoMapperProfiles;
+using GBertolini.UsersService.Models.Dto;
+using GBertolini.UsersService.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBertolini.UsersService.Business.UnitTests.Helpers
+{
+    public static class TestHelper
+    {
+        /// <summary>
+        /// Prepares the environment
+        /// </summary>
+        public static void PrepareEnvironment()
+        {
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "UnitTesting");
+        }
+
+        /// <summary>
+        /// Creates an instance of UserDto using example data
+        /// </summary>
+        public static UserDto CreateUserDto(decimal money, UserType userType)
+            => new UserDto
+            {
+                Name = "German Bertolini",
+                Address = "Buenos Aires, Argentina",
+                Email = "german.bertolini@gmail.com",
+                Money = money,
+                Phone = "1234-1234",
+                UserType = userType
+            };
+
+        /// <summary>
+        /// Instance AutoMapper using the same configuration than injected version
+        /// </summary>
+        public static IMapper InstanceAutoMapper()
+            => MapperConfig.InitializeAutoMapper().CreateMapper();
+    }
+}
---
+++ tests/GBertolini.UsersService.Business.UnitTests/UsersBusinessTest.cs
@@ -0,0 +1,40 @@
+using AutoMapper;
+using GBertolini.UsersService.Business.UnitTests.Helpers;
+using GBertolini.UsersService.Business.Users.Implementation;
+using GBertolini.UsersService.DataAccess;
+using GBertolini.UsersService.DataAccess.Repositories;
+using GBertolini.UsersService.Models.Enums;
+using FluentAssertions;
+using GBertolini.UsersService.Business.Exceptions;
+
+namespace GBertolini.UsersService.Business.UnitTests
+{
+    public class UsersBusinessTest
+    {
+        private readonly IMapper _mapper;
+
+        public UsersBusinessTest()
+        {
+            TestHelper.PrepareEnvironment();
+            _mapper = TestHelper.InstanceAutoMapper();
+        }
+
+        [Fact]
+        public async Task Create_NonExistentUser_FinishesOk()
+        {
+            //Arrange
+            var dto = TestHelper.CreateUserDto(money: 120, userType: UserType.Normal);
+            var dbContext = new UsersDbContext();
+            var repository = new UsersRepository(dbContext);
+            var business = new UsersBusiness(_mapper, repository);
+
+            //Act
+            Func<Task> act = async () => {
+                await business.CreateAsync(dto);
+            };
+
+            //Assert
+            act.Should().NotThrowAsync<BusinessException>();
+        }
+    }
+}
